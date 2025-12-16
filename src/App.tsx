4c75ddf99fcc1253771ca2e4e0f2f9171159099a
StewardRighteous <110706656+StewardRighteous@users.noreply.github.com.>
2025-12-16 19:24:07 +0530

    make remove background transparent

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { pipeline } from "@huggingface/transformers";
+import Files from "./Files";
+import { cropTransparentImage } from "./utils/cropTransparentImage";
 
 type ModelType = "briaai/RMBG-1.4" | "Xenova/modnet";
 
@@ -17,10 +19,13 @@ export default function App() {
       const segmenter = await pipeline("background-removal", model, {
         dtype: "q8",
       });
+
       const imageUrl = URL.createObjectURL(file);
       const output = await segmenter(imageUrl);
-      const blob = await output[0].toBlob();
-      setOutputImage(URL.createObjectURL(blob));
+      const bgRemovedBlob = await output[0].toBlob();
+      const bgRemovedUrl = URL.createObjectURL(bgRemovedBlob);
+      const croppedBlob = await cropTransparentImage(bgRemovedUrl);
+      setOutputImage(URL.createObjectURL(croppedBlob));
     } catch (error) {
       console.error("Error removing background:", error);
     } finally {
@@ -29,52 +34,49 @@ export default function App() {
   }
 
   return (
-    <div className="container">
-      <h2>AI Background Remover</h2>
-
-      <select
-        name="model"
-        id="model"
-        value={model}
-        onChange={(e) =>
-          setModel(
-            e.target.value === "briaai/RMBG-1.4"
-              ? "briaai/RMBG-1.4"
-              : "Xenova/modnet"
-          )
-        }
-      >
-        <option value="briaai/RMBG-1.4">Model 1</option>
-        <option value="Xenova/modnet">Model 2</option>
-      </select>
-
-      <input
-        type="file"
-        accept="image/*"
-        onChange={(e) => setFile(e.target.files?.[0] || null)}
-      />
-
-      <button onClick={removeBackground} disabled={!file || loading}>
-        {loading ? "Processing..." : "Remove Background"}
-      </button>
-
-      <hr />
-
-      <div className="images">
-        {file && (
-          <div className="image-container">
-            <p>Original:</p>
-            <img src={URL.createObjectURL(file)} alt="Original" />
-          </div>
-        )}
-
-        {outputImage && (
-          <div className="image-container">
-            <p>Result:</p>
-            <img src={outputImage} alt="Background Removed" />
-          </div>
-        )}
+    <>
+      <div className="container">
+        <h2>AI Background Remover</h2>
+        <select
+          name="model"
+          id="model"
+          value={model}
+          onChange={(e) =>
+            setModel(
+              e.target.value === "briaai/RMBG-1.4"
+                ? "briaai/RMBG-1.4"
+                : "Xenova/modnet"
+            )
+          }
+        >
+          <option value="briaai/RMBG-1.4">Model 1</option>
+          <option value="Xenova/modnet">Model 2</option>
+        </select>
+        <input
+          type="file"
+          accept="image/*"
+          onChange={(e) => setFile(e.target.files?.[0] || null)}
+        />
+        <button onClick={removeBackground} disabled={!file || loading}>
+          {loading ? "Processing..." : "Remove Background"}
+        </button>
+        <hr />
+        <div className="images">
+          {file && (
+            <div className="image-container">
+              <p>Original:</p>
+              <img src={URL.createObjectURL(file)} alt="Original" />
+            </div>
+          )}
+          {outputImage && (
+            <div className="image-container">
+              <p>Result:</p>
+              <img src={outputImage} alt="Background Removed" />
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+      {outputImage && <Files image={outputImage} />}
+    </>
   );
 }
